ea8776da1d05a2add48c0edb637a3dbd4789baf9
Simon Frid <simon.frid@gmail.com>
2016-10-12 11:05:51 -0700

    Add Estimators and DataSets persistant module

--- estimators/datasets.py
+++ estimators/datasets.py
@@ -1,11 +1,25 @@
+from .database import Base, HashableFileMixin
 
 
-class DataSet():
+class DataSet(HashableFileMixin, Base):
 
     """docstring for DataSet"""
+    ROOT_DIR = 'files/datasets'
+    _object_property_name = '_data'
+    _data = None
+
+    __tablename__ = 'dataset'
 
     def __init__(self, **options):
         self.data = options.pop('data', None)
 
-        if options:
-            raise ValueError("Unexpected kw arguments: %r" % options.keys())
+    @property
+    def data(self):
+        return self.get_object()
+
+    @data.setter
+    def data(self, obj):
+        self.set_object(obj)
+
+    def __repr__(self):
+        return '<Dataset(id=%s hash=%s)>' % (self.id, self.hash)
--- estimators/estimators.py
+++ estimators/estimators.py
@@ -1,11 +1,25 @@
+from .database import Base, HashableFileMixin
 
 
-class Estimator:
+class Estimator(HashableFileMixin, Base):
 
     """docstring for Estimators"""
+    ROOT_DIR = 'files/estimators'
+    _object_property_name = '_estimator'
+    _estimator = None
+
+    __tablename__ = 'estimator'
 
     def __init__(self, **options):
         self.estimator = options.pop('estimator', None)
 
-        if options:
-            raise ValueError("Unexpected kw arguments: %r" % options.keys())
+    @property
+    def estimator(self):
+        return self.get_object()
+
+    @estimator.setter
+    def estimator(self, obj):
+        self.set_object(obj)
+
+    def __repr__(self):
+        return '<Estimator(id=%s hash=%s %s)>' % (self.id, self.hash, self.estimator)
